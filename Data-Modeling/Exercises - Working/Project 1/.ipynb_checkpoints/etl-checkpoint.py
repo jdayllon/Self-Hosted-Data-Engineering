ebc90d0356b91e2c4fc65edc22df098b4c82dd07
Juan David <jdayllon@gmail.com>
2020-04-21 21:11:41 +0200

    Commit pending changes

--- Data-Modeling/Exercises - Working/Project 1/.ipynb_checkpoints/etl-checkpoint.py
+++ Data-Modeling/Exercises - Working/Project 1/.ipynb_checkpoints/etl-checkpoint.py
@@ -6,6 +6,67 @@ import numpy as np
 from sql_queries import *
 
 
+def get_files(filepath):
+    all_files = []
+    for root, dirs, files in os.walk(filepath):
+        files = glob.glob(os.path.join(root,'*.json'))
+        for f in files :
+            all_files.append(os.path.abspath(f))
+    
+    return all_files
+
+def song_table_insert(cur, data):
+    # Insert row
+    try: 
+        cur.execute(insert_songs, list(data.values))
+        cur.connection.commit()
+        return True
+    except psycopg2.Error as e:
+        print("Error: Could not create a row")
+        print(e)
+        cur.connection.rollback()
+        
+        return False
+
+def artist_table_insert(cur, data):
+    # Insert row
+    try: 
+        cur.execute(insert_artist, list(data.values))
+        cur.connection.commit()
+        return True
+    except psycopg2.Error as e:
+        print(f"Error: Could not create a row: {data.values}")
+        print(e)
+        cur.connection.rollback()
+        
+        return False
+
+def time_table_insert(cur, data):
+    # Insert row
+    try: 
+        cur.execute(insert_time, list(data.values))
+        cur.connection.commit()
+        return True
+    except psycopg2.Error as e:
+        print("Error: Could not create a row")
+        print(e)
+        cur.connection.rollback()
+        
+        return False
+    
+def user_table_insert(cur, data):
+    # Insert row
+    try: 
+        cur.execute(insert_users, list(data.values))
+        cur.connection.commit()
+        return True
+    except psycopg2.Error as e:
+        print(f"Error: Could not create a row: {data.values}")
+        print(e)
+        cur.connection.rollback()
+        
+        return False
+
 def process_song_file(cur, filepath):
     """
         This function reads JSON files and read information of song and artist data and saves into song_data and artist_data
@@ -14,7 +75,19 @@ def process_song_file(cur, filepath):
         filepath: location of JSON files
         Return: None
     """
-    pass
+    song_files = [cf for cf in get_files('data/song_data/') if "ipynb" not in cf]
+    df_song_file = []
+
+    for cf in song_files:
+        df_song_file += [pd.read_json(cf,lines=True)]
+
+    df_song_file = pd.concat(df_song_file).reset_index().drop('index',1)
+    df_song_data = df_song_file[['song_id','title','artist_id','year','duration']]
+    
+    df_song_data.apply(lambda d: song_table_insert(cur,d),1)
+    
+    df_artist_data = df_song_file[['artist_id','artist_name','artist_location','artist_latitude','artist_longitude']]
+    df_artist_data.apply(lambda d: artist_table_insert(cur,d),1)
 
 
 def process_log_file(cur, filepath):
@@ -26,17 +99,59 @@ def process_log_file(cur, filepath):
         Return: None
     """
 
-   pass
+   log_files = [cf for cf in get_files('data/log_data/') if "ipynb" not in cf]
+
+    df_log_file = []
+
+    for cf in log_files:
+        df_log_file += [pd.read_json(cf,lines=True)]
+
+    df_log_file = pd.concat(df_log_file).reset_index().drop('index',1).query("page == 'NextSong'")
+    df_log_file.head()
+
+    df_log_file['ts_date'] = df_log_file['ts'].apply(lambda x: datetime.fromtimestamp(x / 1000))
+
+    column_labels = list(('start_time', 'hour', 'day', 'week', 'month', 'year', 'weekday'))
+
+    time_df = pd.DataFrame(
+        df_log_file['ts_date'].apply(lambda x: (x, x.hour, x.day, x.week, x.month, x.year, x.weekday())).tolist(),
+        columns = column_labels
+    )
+    
+    time_df.apply(lambda d: time_table_insert(cur,d),1)
+    
+    user_df = df_log_file[['userId','firstName','lastName','gender','level']]
+    user_df = user_df.drop(user_df.query('userId == ""').index)
+    user_df['userId'] = user_df['userId'].apply(lambda x: int(x))
+    user_df = user_df.sort_values(by=['userId']).drop_duplicates()
+    
+    user_df.apply(lambda d: user_table_insert(cur,d),1)
 
 
 def process_data(cur, conn, filepath, func):
-
-    pass
-
+    
+    func(cur, filepath)
 
 def main():
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
+    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=sparkify password=sparkify")
     cur = conn.cursor()
+    
+    # Prepare Database
+    # Droping tables
+    for cur_table in drop_table_queries:    
+        try:             
+            cur.execute(cur_table)
+        except psycopg2.Error as e:
+            print("Error: Could not create a table")
+            print(e)
+            
+    # Creating tables
+    for cur_table in create_table_queries:    
+        try:             
+            cur.execute(cur_table)
+        except psycopg2.Error as e:
+            print("Error: Could not create a table")
+            print(e)    
 
     process_data(cur, conn, filepath='data/song_data', func=process_song_file)
     process_data(cur, conn, filepath='data/log_data', func=process_log_file)
--- Data-Modeling/Exercises - Working/Project 1/.ipynb_checkpoints/sql_queries-checkpoint.py
+++ Data-Modeling/Exercises - Working/Project 1/.ipynb_checkpoints/sql_queries-checkpoint.py
@@ -1,11 +1,107 @@
 # DROP TABLES
 
+drop_table_songplays = "DROP TABLE IF EXISTS songplays"
+drop_table_songs = "DROP TABLE IF EXISTS songs"
+drop_table_artist =  "DROP TABLE IF EXISTS artists"
+drop_table_time = "DROP TABLE IF EXISTS time"
+drop_table_users = "DROP TABLE IF EXISTS users"
 
 # CREATE TABLES
 
+create_table_users = """
+CREATE TABLE IF NOT EXISTS users
+(
+    user_id SMALLINT PRIMARY KEY,
+    first_name TEXT,
+    last_name TEXT,
+    gender VARCHAR(1),
+    level VARCHAR(4)
+)"""
+
+create_table_songs = """
+CREATE TABLE IF NOT EXISTS songs
+(
+    song_id VARCHAR(18) PRIMARY KEY,
+    title TEXT,
+    artist_id VARCHAR(18),
+    year SMALLINT,
+    duration NUMERIC
+)"""
+
+create_table_artist = """
+CREATE TABLE IF NOT EXISTS artists
+(
+    artist_id VARCHAR(18) PRIMARY KEY,
+    name TEXT,
+    location TEXT,
+    latitude NUMERIC,
+    longitude NUMERIC
+)"""
+
+create_table_time = """
+CREATE TABLE IF NOT EXISTS time
+(
+    start_time TIMESTAMP PRIMARY KEY,
+    hour SMALLINT,
+    day SMALLINT,
+    week SMALLINT,
+    month SMALLINT,
+    year SMALLINT,
+    weekday SMALLINT
+)"""
+
+create_table_songplays = """
+CREATE TABLE IF NOT EXISTS songplays
+(
+    songplay_id SERIAL PRIMARY KEY,
+    start_time TIMESTAMP REFERENCES time(start_time),
+    user_id SMALLINT REFERENCES users(user_id),
+    level VARCHAR(4),
+    song_id VARCHAR(18),
+    artist_id VARCHAR(18),
+    session_id SMALLINT,
+    location TEXT,
+    user_agent TEXT
+)"""
+
 # INSERT RECORDS
+
+## https://www.postgresql.org/docs/9.5/sql-insert.html
+
+insert_songs = """
+INSERT INTO songs (song_id, title, artist_id, year, duration) \
+VALUES (%s,%s,%s,%s,%s)
+"""
+insert_artist = """
+INSERT INTO artists (artist_id, name, location, latitude, longitude) \
+VALUES (%s,%s,%s,%s,%s)
+"""
+
+insert_time = """
+INSERT INTO time (start_time, hour, day, week, month, year, weekday) \
+VALUES (%s,%s,%s,%s,%s,%s,%s)
+"""
+
+insert_users = """
+INSERT INTO users (user_id, first_name, last_name, gender, level) \
+VALUES (%s,%s,%s,%s,%s)
+"""
+insert_songplays = """
+INSERT INTO songplays (start_time, user_id, level, song_id, artist_id,session_id,location,user_agent) \
+VALUES (%s,%s,%s,%s,%s,%s,%s,%s)
+"""
 
 # FIND SONGS
 
+query_songs = """
+SELECT songs.song_id, songs.artist_id
+FROM songs
+JOIN artists on songs.artist_id = artists.artist_id
+WHERE songs.title = %s
+AND artists.name = %s
+AND songs.duration = %s
+"""
 
 # QUERY LISTS
+create_table_queries = [create_table_users, create_table_songs, create_table_artist, create_table_time, create_table_songplays]
+drop_table_queries = [drop_table_users, drop_table_time, drop_table_artist, drop_table_songs, drop_table_songplays]
